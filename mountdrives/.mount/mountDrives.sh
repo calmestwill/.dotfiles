859cdc5406d1df43fecabcf0fc33739f54090a94
Will <will.b.spooner@gmail.com>
2020-01-25 13:14:49 +0000

    Code cleanup

--- mountdrives/.mount/mountDrives.sh
+++ mountdrives/.mount/mountDrives.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
-CYAN='\033[0;36m'
+CYAN='\033[0;36m' # Colors for use in the program
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 
 echo -e "\n${YELLOW}Welcome back!"
-if [[ "$(hostname)" == "icarus" ]]; then
+
+if [[ "$(hostname)" == "icarus" ]]; then # If running on PC
+
+	# Mount the bitlocker encrypted drives with dislocker, passphrase file must be present
 	echo -e "${CYAN}Mounting encrypted data drive (Andromeda)..."
 	sudo dislocker /dev/sdb1 -u"$(cat ~/.mount/andromedaPassphrase.txt)" -- /media/bitlockerdecrypt/Andromeda
 	echo "Drive decrypted, mounting..."
 	sudo mount -o loop,rw /media/bitlockerdecrypt/Andromeda/dislocker-file /media/Andromeda
-
 	echo -e "\nMounting encrypted data drive (Gemini)..."
 	sudo dislocker /dev/sdd1 -u"$(cat ~/.mount/geminiPassphrase.txt)" -- /media/bitlockerdecrypt/Gemini
 	echo "Drive decrypted, mounting..."
 	sudo mount -o loop,rw /media/bitlockerdecrypt/Gemini/dislocker-file /media/Gemini
-
 	echo -e "\n${GREEN}Success!"
-elif [[ "$(hostname)" == "daedalus" ]]; then
+
+elif [[ "$(hostname)" == "daedalus" ]]; then # If running on laptop
+	
+	# Mount the encrypted partition with veracrypt, keyfiles and passphrase must be present
 	echo -e "${CYAN}Mounting encrypted data partition..."
 	sudo veracrypt -t /dev/nvme0n1p7 -p "$(cat ~/.mount/pegasusPassphrase.txt)" -k ~/.mount/Pegasus /media/Pegasus --non-interactive
 	echo -e "\n${GREEN}Success!"
+
 else
+
 	echo -e "No drives for hostname. Have you set the correct hostname? \n"
+
 fi
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,4 +1,7 @@
+# Mount encrypted drives on login
 ~/.mount/mountDrives.sh
+
+# Start sway upon login to tty1
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
   XKB_DEFAULT_LAYOUT=us exec sway
 fi
