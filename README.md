75aa84f9aeaadae87c003597fa6601528442af5f
calmestwill <15276500+calmestwill@users.noreply.github.com>
2020-01-26 08:18:40 +0000

    Setup will now give option to install packages

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 My Arch Linux dotfiles for the Sway tiling window manager. I use GNU Stow to handle the symlinking.
 
 ## Installation
-You can use the included `setup.sh` script to easily install:
+You can use the included `setup.sh` script to easily install. It will give the option to install some of the main packages automatically, otherwise it will just install stow and create the symlinks.
 ```shell
+git clone https://github.com/calmestwill/.dotfiles.git
+cd .dotfiles
 ./setup.sh
 ```
 
--- setup.sh
+++ setup.sh
@@ -2,7 +2,28 @@
 
 YELLOW='\033[1;33m' # Colors for use in the program
 GREEN='\033[0;32m'
+RESET='\033[0m'
 
-echo -e "${YELLOW}Creating symlinks... " # Use stow to create the symlinks
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root! "
+    exit
+fi
+
+echo -e "${YELLOW}Updating package repositories...${RESET}"
+pacman -Syy
+
+read -p "Would you like to install main packages? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+    echo -e "${YELLOW}Installing main packages...${RESET}"
+    pacman -S sway ranger rofi sway vim waybar rxvt-unicode zsh stow
+
+else
+    pacman -S stow
+fi
+
+
+echo -e "${YELLOW}Creating symlinks...${RESET}" # Use stow to create the symlinks
 stow -v mountdrives ranger rofi sway vim waybar xdefaults zsh
 echo -e "${GREEN}Done! "
